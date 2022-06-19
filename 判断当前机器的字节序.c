121e4f8ec3e0bf69aacab3f98ab597ff06e12bf4
Haotian Li <2878348186@qq.com>
2022-06-19 17:54:44 +0800

    Create 判断当前机器的字节序.c

---
+++ 判断当前机器的字节序.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int check_sys()
+{
+	int a = 1;
+	//返回1，小端
+	//返回0，大端
+	return *(char*)&a;
+}
+
+int main()
+{
+	int ret = check_sys();
+	if (ret == 1)
+	{
+		printf("小端\n");
+	}
+	else
+	{
+		printf("大端\n");
+	}
+	return 0;
+}
