65897555c3306dd16c151e7f713c895007f47eb1
Li Haotian <2878348186@qq.com>
2022-08-29 12:29:32 +0800

    Create ding.bat

---
+++ adb实现钉钉智能填表/日常打卡/ding.bat
@@ -0,0 +1,47 @@
+::修改cmd编码为UTF-8
+chcp 65001
+
+::电源键
+adb shell input keyevent 224
+timeout /T 1
+
+::上滑
+adb shell input swipe 540 1800 540 800 100
+timeout /T 1
+
+::取消USB连接弹窗
+adb shell input tap 540 2220
+timeout /T 1
+
+::HOME键
+adb shell input keyevent 3
+
+::点击钉钉图标
+adb shell input tap 930 2250
+timeout /T 5
+
+::钉钉内点击
+adb shell input tap 540 710
+timeout /T 5
+adb shell input tap 540 2220
+timeout /T 5
+
+::上划
+adb shell input swipe 540 1800 540 800 100
+timeout /T 3
+
+::点击获取定位
+adb shell input tap 1000 1220
+timeout /T 5
+
+::提交
+adb shell input tap 540 2260
+timeout /T 3
+
+::提示信息
+type "E:\Desktop\钉钉自动打卡\1.txt"
+timeout /T 5
+::手机重启
+adb reboot
+::电脑关机
+shutdown -s -t 3
